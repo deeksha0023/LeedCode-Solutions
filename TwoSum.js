314d9bacff53b60cf3f3e105e85c2925860c01fb
Deeksha yadav <deekshayadav1029@gmail.com>
2026-04-19 04:54:25 +0530

    Day 1: Two Sum Solution
    Solved using Hashmap with O(n) time complexity.

---
+++ TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let need = target - nums[i];
+        if (need in map) {
+            return [map[need], i];
+        }
+        map[nums[i]] = i;
+    }
+};
